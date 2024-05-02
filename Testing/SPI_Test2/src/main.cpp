f718f64c17e56b863fb06c6a8d966e3a3c1629a4
Khu0882 <130413800+Khu0882@users.noreply.github.com>
2024-05-01 21:09:39 -0700

    its giving
    its giving but its the wrong thing ):
    
    0xbc950360

--- Testing/SPI_Test2/src/main.cpp
+++ Testing/SPI_Test2/src/main.cpp
@@ -2,23 +2,23 @@
 #include <SPI.h>
 
 // Define which pins we're using
-// #define DWM1000_SCK_PIN 13
-// #define DWM1000_MISO_PIN 12
-// #define DWM1000_MOSI_PIN 9
-// #define DWM1000_SS_PIN 10
-#define DWM1000_SCK_PIN 14
+#define DWM1000_SCK_PIN 13
 #define DWM1000_MISO_PIN 12
-#define DWM1000_MOSI_PIN 13
-#define DWM1000_SS_PIN 15
+#define DWM1000_MOSI_PIN 11
+#define DWM1000_SS_PIN 10
+// #define DWM1000_SCK_PIN 14
+// #define DWM1000_MISO_PIN 12
+// #define DWM1000_MOSI_PIN 13
+// #define DWM1000_SS_PIN 15
 
 // Define SPI settings
-#define BITORDER SPI_LSBFIRST
-#define FREQUENCY 1000000
+#define BITORDER SPI_MSBFIRST
+#define FREQUENCY 500000
 #define DATA_MODE SPI_MODE3
 
 //helper function prototypes
 uint32_t DWM1000read(uint8_t header);
-uint32_t DWM1000write(uint8_t header);
+void DWM1000write(uint8_t header);
 
 SPIClass MySPI;
 
@@ -38,8 +38,8 @@ void setup() {
 
 void loop() {
   uint32_t result = DWM1000read(0x00); //read WHO_AM_I
-  Serial.println(result);
-  delay(3);
+  Serial.printf("result: 0x%x\n", result);
+  delay(1000);
 }
 
 void DWM1000write(uint8_t register_id, uint32_t data){
@@ -63,7 +63,7 @@ uint32_t DWM1000read(uint8_t register_id){
   MySPI.transfer(header);
   uint32_t dataIn = 0;
   for(int i=0; i<4; ++i){
-    dataIn |= MySPI.transfer((0)) << (i * 8);
+    dataIn |= MySPI.transfer((0xAA)) << (i * 8);
   }
   digitalWrite(MySPI.pinSS(), HIGH);
   MySPI.endTransaction();
